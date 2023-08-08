ba3ba54aac3d2d6a8cd7bb0843470031a5913878
Cashton Holbert <cashtoncoleholbert@gmail.com>
2023-08-07 18:44:06 -0700

    pretty UI and game reset option

---
+++ README.md
@@ -0,0 +1,15 @@
+Rock Paper Scissors Game against an AI Rock-Paper-Scissors Champion
+
+live: 
+
+iOS 16 icons by [icons8](https://icons8.com):
+- [rock](https://icons8.com/icon/37409/rock)
+- [paper](https://icons8.com/icon/6552/paper)
+- [scissors](https://icons8.com/icon/37828/scissors)
+
+Lessons learned:
+- eventListener addition and removal
+- js closure property and event (found in onClickHandler)
+- css image transitions
+
+Expanded on from Odin Project Foundations Course: [Find lesson here](https://www.theodinproject.com/lessons/foundations-revisiting-rock-paper-scissors)
--- myscript.js
+++ myscript.js
@@ -1,15 +1,11 @@
 const WINNING_SCORE = 5;
+
+// all of these globals could be a class, with an object passed around to the functions
+// for this simple project, globals are just fine
 let scorePlayer = 0;
 let scoreComp = 0;
-const buttons = document.querySelectorAll('input');
-/*
-1. prompt user for input being
-2. if not rock, paper, or scissors, then alert user to retry with correct input
-3. get the computer's choice 
-4. compare them, if they are equal, alert a tie, if player wins declare the winner as player like so,
-    "You Win! 'Rock' beats 'Scissors'"
-*/
-
+const choices = document.querySelectorAll('img');
+const roundMessage = document.querySelector('.roundOutcome');
 
 
 function getComputerChoice() {
@@ -30,44 +26,73 @@ function playRound(choicePlayer, choiceComp){
     // check for tie
     if(choicePlayer === choiceComp)
     {
-        return
+        roundMessage.textContent = `You both chose ${choicePlayer}, it's a tie!`;
+        return;
     }
-
-
 
     // if player wins
     if((choicePlayer === "rock" && choiceComp === "scissors") || (choicePlayer === "paper" && choiceComp === "rock") || (choicePlayer === "scissors" && choiceComp === "paper")){
         scorePlayer += 1;
         let scorePlayerElement = document.querySelector('#scorePlayer')
         scorePlayerElement.textContent = `Player Score: ${scorePlayer}`;
+        roundMessage.textContent = `You won this round with ${choicePlayer}`;
+
     }else{
+        // if comp wins
         scoreComp += 1;
         let scoreCompElement = document.querySelector('#scoreComputer')
         scoreCompElement.textContent = `Computer Score: ${scoreComp}`;
+        roundMessage.textContent = `Computer won this round with ${choiceComp}`;
     }
-}
 
-function checkWin(){
+    // check for winner
     if(scorePlayer >= WINNING_SCORE){
-        document.querySelector('#winMessage').textContent = "Player Won!";
-        let buttonElem = document.querySelectorAll('input');
-        buttonElem.forEach(elem => elem.disabled = true);
-        //document.querySelectorAll('input').disabled = true;
+        document.querySelector('.winMessage').textContent = "You Win!";
+        choices.forEach(elem => elem.removeEventListener('click', onClickHandler));
+
+        let resetElem = document.querySelector('.resetButton');
+        resetElem.innerHTML = "<button onclick='resetGame()'>Play Again!</button>";
     }else{
         if(scoreComp >= WINNING_SCORE){
-            document.querySelector('#winMessage').textContent = "Computer Won!";
-            let buttonElem = document.querySelectorAll('input');
-            buttonElem.forEach(elem => elem.disabled = true);
+            document.querySelector('.winMessage').textContent = "Computer Won";
+            choices.forEach(elem => elem.removeEventListener('click', onClickHandler));
+
+            let resetElem = document.querySelector('.resetButton');
+            resetElem.innerHTML = "<button onclick='resetGame()'>Play Again!</button>";
         }
     }
 }
 
+function resetGame() {
 
+    // reset scores locally and in DOM
+    scorePlayer = 0;
+    scoreComp = 0;
 
-buttons.forEach((button) =>
-{
-    button.addEventListener('click', function(){
-        playRound(button.value, getComputerChoice());
-        checkWin();
+    let scorePlayerElement = document.querySelector('#scorePlayer')
+    scorePlayerElement.textContent = `Player Score: ${scorePlayer}`;
+    
+    let scoreCompElement = document.querySelector('#scoreComputer')
+    scoreCompElement.textContent = `Computer Score: ${scoreComp}`;
+
+    roundMessage.textContent = "";
+    document.querySelector('.winMessage').textContent = ""; 
+    document.querySelector('.resetButton').innerHTML = "";
+
+    main();
+}
+
+// need this wrapper so I can remove the event listener easily
+function onClickHandler(event) {
+    const button = event.target;
+    
+    playRound(button.getAttribute('id'), getComputerChoice());
+}
+
+function main() {
+    choices.forEach((button) => {
+        button.addEventListener('click', onClickHandler);
     });
-});
+}
+
+main();
